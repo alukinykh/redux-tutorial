e7a240a05d3253d9fb35d8a53e45dfc66a869558
Anna Lukinykh <alukinykh@crystalnix.com>
2017-09-11 20:23:28 +0600

    Add connect data

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div>Hello world</div>
+    return <div>Hello world, { this.props.user }</div>
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(App)
